e8d8a7cd3a8d5c4bb2c8663abe699e8b3061aca9
Rupesh <rkshandilya001@gmail.com>
2025-03-27 22:44:07 +0530

    Ws, http backend with next frontend

---
+++ apps/backend/index.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import { prismaClient } from "db/client";
+const app = express();
+app.use(express.json());
+
+app.get("/users", async (req, res) => {
+    prismaClient.user.findMany()
+    .then((users: any) => {
+        res.json(users);
+    }).catch((err:any) => {
+        res.status(500).json({error: err});
+    })
+})
+
+app.post("/user", (req, res) => {
+    const { username, password } = req.body;
+    if(!username || !password){
+        res.status(400).json({error: "Username and password are required"});
+        return;
+    }
+    prismaClient.user.create({
+        data: {
+            username,
+            password
+        }
+    }).then((user: any) => {
+        res.status(201).json(user);
+        return;
+    })
+    .catch((err: any) => {
+        res.status(500).json({error: err});
+        return;
+    })
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
