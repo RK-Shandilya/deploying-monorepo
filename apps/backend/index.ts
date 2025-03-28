c0d7d6009c1f5e47a84ca7f4255d900d60fc5ef7
Rupesh <rkshandilya001@gmail.com>
2025-03-28 10:24:26 +0530

    Added Dockerfiles

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -33,6 +33,6 @@ app.post("/user", (req, res) => {
     })
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
+app.listen(3001, () => {
+    console.log("Server is running on http://localhost:3001");
 });
